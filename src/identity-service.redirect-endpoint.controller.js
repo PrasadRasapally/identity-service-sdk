ec315b98d80e4777592dca9dbeb1953ef64af4be
Chris Dostert <chris.dostert@live.com>
2015-05-12 13:37:16 -0700

    added logging

--- src/identity-service.redirect-endpoint.controller.js
+++ src/identity-service.redirect-endpoint.controller.js
@@ -11,19 +11,20 @@
         ]);
 
     function IdentityServiceRedirectEndpoint(identityServiceClient,
-                                                          $location) {
+                                             $location) {
 
         // consume access token parameter
         var accessTokenParameterName = 'access_token';
         var accessToken = $location.search()[accessTokenParameterName];
-        console.log(accessToken);
         identityServiceClient.setAccessToken(accessToken);
         $location.search(accessTokenParameterName, null);
+        console.log('access_token with signature ' + accessToken.split('.')[2] + ' processed');
 
         // consume returnPath parameter
         var returnPathParameterName = 'return_path';
         var returnPath = $location.search()[returnPathParameterName];
         $location.search(returnPathParameterName, null);
+        console.log('return_path ' + returnPath + 'parameter received');
 
         // go to returnPath
         $location.path(returnPath);
