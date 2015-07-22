6d2e6fd841fcb1924679ecb4422645011e1010fe
Chris Dostert <chris.dostert@live.com>
2015-07-22 11:58:53 -0700

    cleanup

--- dist/identity-service-angularjs-sdk.js
+++ dist/identity-service-angularjs-sdk.js
@@ -173,12 +173,10 @@
         var accessTokenParameterName = 'access_token';
         var accessToken = $location.search()[accessTokenParameterName];
         identityServiceClient.setAccessToken(accessToken);
-        console.log('access_token with signature ' + accessToken.split('.')[2] + ' processed');
 
         // consume returnPath parameter
         var returnPathParameterName = 'return_path';
         var returnPath = $location.search()[returnPathParameterName];
-        console.log('return_path ' + returnPath + ' processed');
 
         // redirect to returnPath & replace current browser history record
         // note: at time of writing this but is still open: https://github.com/angular/angular.js/issues/12168
