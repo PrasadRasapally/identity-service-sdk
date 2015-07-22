aa1544cfacbe3b2602b5ae264c0b9f6ff7fb7f14
Chris Dostert <chris.dostert@live.com>
2015-07-22 12:06:52 -0700

    added proper debug logging

--- dist/identity-service-angularjs-sdk.js
+++ dist/identity-service-angularjs-sdk.js
@@ -157,26 +157,30 @@
 (function () {
 
     angular
-        .module('identityServiceModule')
+        .module("identityServiceModule")
         .controller(
-        'IdentityServiceRedirectEndpointController',
+        "IdentityServiceRedirectEndpointController",
         [
-            'identityServiceClient',
-            '$location',
+            "identityServiceClient",
+            "$location",
+            "$log",
             IdentityServiceRedirectEndpointController
         ]);
 
     function IdentityServiceRedirectEndpointController(identityServiceClient,
-                                                       $location) {
+                                                       $location,
+                                                       $log) {
 
         // consume access token parameter
-        var accessTokenParameterName = 'access_token';
+        var accessTokenParameterName = "access_token";
         var accessToken = $location.search()[accessTokenParameterName];
         identityServiceClient.setAccessToken(accessToken);
+        $log.debug("access_token with signature " + accessToken.split(".")[2] + " processed");
 
         // consume returnPath parameter
-        var returnPathParameterName = 'return_path';
+        var returnPathParameterName = "return_path";
         var returnPath = $location.search()[returnPathParameterName];
+        $log.debug("return_path " + returnPath + " processed");
 
         // redirect to returnPath & replace current browser history record
         // note: at time of writing this but is still open: https://github.com/angular/angular.js/issues/12168
