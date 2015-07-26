943c49312b325832f05cc935c91f1cc44cdae7d8
Chris Dostert <chris.dostert@live.com>
2015-07-25 23:18:15 -0700

    test

--- dist/identity-service-angularjs-sdk.js
+++ dist/identity-service-angularjs-sdk.js
@@ -209,33 +209,33 @@
                 baseUrl: objectUnderConstruction.baseUrl,
                 samlIdpUrl: objectUnderConstruction.samlIdpUrl,
                 getSsoLoginUrl: getSsoLoginUrl
+            };
+
+            /**
+             * Gets a URL for initiating a SSO login flow and persists the resulting access_token to browser storage.
+             * @param {string} returnPath - The angular path to route to following successful login
+             */
+            function getSsoLoginUrl(returnPath) {
+
+                // the url to return to after successfully logging in.
+                var relayState = $window.location.protocol
+                    + "//"
+                    + $window.location.host
+                    + $window.location.pathname
+                    + "#/identity-service/redirect-endpoint?access_token={access_token}&return_path="
+                    + returnPath;
+
+                // determine appropriate prefix for relay state parameter
+                var relayStatePrefix = "&";
+                if (1 > objectUnderConstruction.samlIdpUrl.indexOf("?")) {
+                    relayStatePrefix = "?";
+                }
+
+                return objectUnderConstruction.samlIdpUrl
+                    + relayStatePrefix
+                    + "RelayState="
+                    + encodeURIComponent(relayState);
             }
-        }
-
-        /**
-         * Gets a URL for initiating a SSO login flow and persists the resulting access_token to browser storage.
-         * @param {string} returnPath - The angular path to route to following successful login
-         */
-        function getSsoLoginUrl(returnPath) {
-
-            // the url to return to after successfully logging in.
-            var relayState = $window.location.protocol
-                + "//"
-                + $window.location.host
-                + $window.location.pathname
-                + "#/identity-service/redirect-endpoint?access_token={access_token}&return_path="
-                + returnPath;
-
-            // determine appropriate prefix for relay state parameter
-            var relayStatePrefix = "&";
-            if (1 > objectUnderConstruction.samlIdpUrl.indexOf("?")) {
-                relayStatePrefix = "?";
-            }
-
-            return objectUnderConstruction.samlIdpUrl
-                + relayStatePrefix
-                + "RelayState="
-                + encodeURIComponent(relayState);
         }
     }
 })();
