f80775b036640ee17358ec5f24398b8e24a72ea7
Chris Dostert <chris.dostert@live.com>
2015-07-29 19:39:02 -0700

    1) moved session related use cases to dedicated angularjs-session module 2) added refreshAccessToken use case

--- dist/identity-service-angularjs-sdk.js
+++ dist/identity-service-angularjs-sdk.js
@@ -7,6 +7,29 @@
             "LocalStorageModule"
         ]);
 })();
+/**
+ * OpenID Connect (OIDC) UserInfo
+ * @typedef {Object} OidcUserInfo
+ * @property {string} given_name
+ * @property {string} family_name
+ * @property {string} email
+ * @property {string} type - either "partnerRep" or "employee"
+ */
+
+/**
+ * OpenID Connect (OIDC) UserInfo for a partner rep
+ * @typedef {Object} PartnerRepOidcUserInfo
+ * @augments {OidcUserInfo}
+ * @property {number} sub
+ * @property {(string|null)} partner_sap_account_number
+ * @property {(string|null)} sap_vendor_number
+ */
+
+/**
+ * OpenID Connect (OIDC) UserInfo for an employee
+ * @typedef {Object} EmployeeOidcUserInfo
+ * @augments {OidcUserInfo}
+ */
 (function () {
     angular
         .module("identityServiceSdk.module")
@@ -18,8 +41,6 @@
 
         var objectUnderConstruction = {
             setBaseUrl: setBaseUrl,
-            setSamlLoginUrl: setSamlLoginUrl,
-            setLogoutUrl: setLogoutUrl,
             $get: $get
         };
 
@@ -30,21 +51,9 @@
             return objectUnderConstruction;
         }
 
-        function setSamlLoginUrl(samlLoginUrl) {
-            objectUnderConstruction.samlLoginUrl = samlLoginUrl;
-            return objectUnderConstruction;
-        }
-
-        function setLogoutUrl(logoutUrl) {
-            objectUnderConstruction.logoutUrl = logoutUrl;
-            return objectUnderConstruction;
-        }
-
         function $get() {
             return {
-                baseUrl: objectUnderConstruction.baseUrl,
-                samlLoginUrl: objectUnderConstruction.samlLoginUrl,
-                logoutUrl: objectUnderConstruction.logoutUrl
+                baseUrl: objectUnderConstruction.baseUrl
             };
 
         }
@@ -56,79 +65,19 @@
         .factory(
         "identityServiceSdk",
         [
-            "identityServiceSdk.getAccessTokenUseCase",
             "identityServiceSdk.getUserInfoUseCase",
-            "identityServiceSdk.loginWithSamlUseCase",
-            "identityServiceSdk.logoutUseCase",
+            "identityServiceSdk.refreshAccessTokenUseCase",
             identityServiceSdk
         ]);
 
-    function identityServiceSdk(getAccessTokenUseCase,
-                                getUserInfoUseCase,
-                                loginWithSamlUseCase,
-                                logoutUseCase) {
+    function identityServiceSdk(getUserInfoUseCase,
+                                refreshAccessTokenUseCase) {
 
         return {
-            getAccessToken: getAccessTokenUseCase.execute,
             getUserInfo: getUserInfoUseCase.execute,
-            loginWithSaml: loginWithSamlUseCase.execute,
-            logout: logoutUseCase.execute
+            refreshAccessToken: refreshAccessTokenUseCase.execute
         }
 
-    }
-})();
-
-(function () {
-    angular
-        .module("identityServiceSdk.module")
-        .factory(
-        "identityServiceSdk.getAccessTokenService",
-        [
-            "localStorageService",
-            getAccessTokenService
-        ]);
-
-    function getAccessTokenService(localStorageService) {
-
-        return {
-            execute: execute
-        };
-
-        /**
-         * Retrieves the current access_token from browser storage. Primarily used to build an
-         * Authorization header to make an API call to a protected resource.
-         * @returns {string}
-         */
-        function execute() {
-            return localStorageService.get("accessToken");
-        }
-    }
-})();
-
-(function () {
-    angular
-        .module("identityServiceSdk.module")
-        .factory(
-        "identityServiceSdk.getAccessTokenUseCase",
-        [
-            "identityServiceSdk.getAccessTokenService",
-            getAccessTokenUseCase
-        ]);
-
-    function getAccessTokenUseCase(getAccessTokenService) {
-
-        return {
-            execute: execute
-        };
-
-        /**
-         * Retrieves the current access_token from browser storage. Primarily used to build an
-         * Authorization header to make an API call to a protected resource.
-         * @returns {string}
-         */
-        function execute() {
-            return getAccessTokenService.execute();
-        }
     }
 })();
 
@@ -139,73 +88,35 @@
         .factory(
         "identityServiceSdk.getUserInfoUseCase",
         [
-            "$location",
             "$http",
             "identityServiceSdk.config",
-            "identityServiceSdk.getAccessTokenService",
-            "identityServiceSdk.loginWithSamlService",
             getUserInfoUseCase
         ]);
 
-    function getUserInfoUseCase($location,
-                                $http,
-                                config,
-                                getAccessTokenService,
-                                loginWithSamlService) {
+    function getUserInfoUseCase($http,
+                                config) {
 
         return {
             execute: execute
         };
 
         /**
-         * OpenID Connect (OIDC) UserInfo
-         * @typedef {Object} OidcUserInfo
-         * @property {string} given_name
-         * @property {string} family_name
-         * @property {string} email
-         * @property {string} type - either "partnerRep" or "employee"
-         */
-
-        /**
-         * OpenID Connect (OIDC) UserInfo for a partner rep
-         * @typedef {Object} PartnerRepOidcUserInfo
-         * @augments {OidcUserInfo}
-         * @property {number} sub
-         * @property {(string|null)} partner_sap_account_number
-         * @property {(string|null)} sap_vendor_number
-         */
-
-        /**
-         * OpenID Connect (OIDC) UserInfo for an employee
-         * @typedef {Object} EmployeeOidcUserInfo
-         * @augments {OidcUserInfo}
-         */
-
-        /**
-         * Gets userInfo for the current user.
+         * Gets userInfo via the provided access token.
+         * @param {String} accessToken
          * @returns a promise of {OidcUserInfo}
          */
-        function execute() {
+        function execute(accessToken) {
 
             return $http({
                 headers: {
-                    Authorization: "Bearer " + getAccessTokenService.execute()
+                    Authorization: "Bearer " + accessToken
                 },
                 method: "get",
                 url: config.baseUrl + "/oauth2/userinfo"
             })
-                .then(function onFulfilled(response) {
+                .then(function (response) {
+
                     return response.data;
-
-                }, function onRejected(response) {
-
-                    // if we're on the redirect endpoint don't re-authenticate; can cause infinite loop
-                    if ($location.path() == "/identity-service/redirect-endpoint") {
-                        return response;
-                    }
-                    else if (response.status === 401) {
-                        loginWithSamlService.execute();
-                    }
 
                 });
         }
@@ -214,228 +125,43 @@
 })();
 
 (function () {
+
     angular
         .module("identityServiceSdk.module")
-        .service("identityServiceSdk.loginUrlFactory",
+        .factory(
+        "identityServiceSdk.refreshAccessTokenUseCase",
         [
-            "$window",
+            "$http",
             "identityServiceSdk.config",
-            loginUrlFactory
+            refreshAccessTokenUseCase
         ]);
 
-    function loginUrlFactory($window,
-                             config) {
-
-        return {
-            construct: construct
-        };
-
-        /**
-         * Constructs a URL for initiating a SSO login flow and persisting the
-         * resulting access_token to browser storage.
-         * @param {string} returnPath - The angular path to route to following successful login
-         */
-        function construct(returnPath) {
-
-            // the url to return to after successfully logging in.
-            var relayState = $window.location.protocol
-                + "//"
-                + $window.location.host
-                + $window.location.pathname
-                + "#/identity-service/redirect-endpoint?access_token={access_token}&return_path="
-                + returnPath;
-
-            // determine appropriate prefix for relay state parameter
-            var relayStatePrefix = "&";
-            if (1 > config.samlLoginUrl.indexOf("?")) {
-                relayStatePrefix = "?";
-            }
-
-            return config.samlLoginUrl
-                + relayStatePrefix
-                + "RelayState="
-                + encodeURIComponent(relayState);
-        }
-    }
-})();
-(function () {
-    angular
-        .module("identityServiceSdk.module")
-        .factory(
-        "identityServiceSdk.loginWithSamlService",
-        [
-            "identityServiceSdk.loginUrlFactory",
-            "$location",
-            "$window",
-            loginWithSamlService
-        ]
-    );
-
-    function loginWithSamlService(loginUrlFactory,
-                          $location,
-                          $window) {
-
-        return {
-            execute: execute
-        };
-
-
-        function execute() {
-
-            $window.location = loginUrlFactory.construct($location.path());
-
-        }
-
-    }
-
-})();
-(function () {
-    angular
-        .module("identityServiceSdk.module")
-        .factory(
-        "identityServiceSdk.loginWithSamlUseCase",
-        [
-            "identityServiceSdk.loginWithSamlService",
-            loginWithSamlUseCase
-        ]);
-
-    function loginWithSamlUseCase(loginWithSamlService) {
+    function refreshAccessTokenUseCase($http,
+                                       config) {
 
         return {
             execute: execute
         };
 
         /**
-         * Logs a user in by:
-         * 1.  initiates a SAML 2.0 SP initiated Redirect POST login flow
-         * 2.  sets the resulting access token in browser storage such that it will be returned
-         * by identityServiceSdk.getCurrentAccessToken
+         * Refreshes the provided access token. Primarily used to refresh an access token for the
+         * purposes of maintaining an active session without redirecting the client browser to re-authenticate.
+         * @param {String} accessToken
+         * @returns a promise of {String}
          */
-        function execute() {
-
-            loginWithSamlService.execute();
-
-        }
-    }
-})();
-(function () {
-    angular
-        .module("identityServiceSdk.module")
-        .factory(
-        "identityServiceSdk.logoutUseCase",
-        [
-            "$window",
-            "identityServiceSdk.config",
-            "localStorageService",
-            logoutUseCase
-        ]);
-
-    /**
-     * Logs out the currently logged in user by:
-     * 1) removing their accessToken from localStorage
-     * 2) redirecting them to the configured logoutUrl
-     */
-    function logoutUseCase($window,
-                           config,
-                           localStorageService) {
-
-        return {
-            execute: execute
-        };
-
-        function execute() {
-
-            localStorageService.remove("accessToken");
-
-            // redirect to logoutUrl
-            $window.location = config.logoutUrl;
-
-        }
-    }
-})();
-(function () {
-
-    angular
-        .module("identityServiceSdk.module")
-        .controller(
-        "identityServiceSdk.RedirectEndpointController",
-        [
-            "identityServiceSdk.setAccessTokenService",
-            "$location",
-            RedirectEndpointController
-        ]);
-
-    function RedirectEndpointController(setAccessTokenService,
-                                        $location) {
-
-        // get search parameters
-        var searchParams = $location.search();
-
-        // replace current browser history record
-        $location.replace();
-
-        // consume access token parameter
-        var accessTokenParameterName = "access_token";
-        var accessToken = searchParams[accessTokenParameterName];
-        setAccessTokenService.execute(accessToken);
-        searchParams[accessTokenParameterName] = null;
-
-        // consume returnPath parameter
-        var returnPathParameterName = "return_path";
-        var returnPath = searchParams[returnPathParameterName];
-        searchParams[returnPathParameterName] = null;
-
-        // redirect to returnPath & replace current browser history record
-        $location.search(searchParams);
-        $location.path(returnPath);
-
-    }
-})();
-
-(function () {
-    angular
-        .module("identityServiceSdk.module")
-        .config(
-        [
-            "$routeProvider",
-            function ($routeProvider) {
-                $routeProvider
-                    .when
-                (
-                    "/identity-service/redirect-endpoint",
-                    {
-                        template: " ",
-                        controller: "identityServiceSdk.RedirectEndpointController",
-                        controllerAs: "controller"
-                    }
-                )
-            }
-        ]);
-})();
-(function () {
-    angular
-        .module("identityServiceSdk.module")
-        .factory(
-        "identityServiceSdk.setAccessTokenService",
-        [
-            "localStorageService",
-            setAccessTokenService
-        ]);
-
-    /**
-     * Sets the current access_token in browser storage.
-     * @param {string} accessToken
-     */
-    function setAccessTokenService(localStorageService) {
-
-        return {
-            execute: execute
-        };
-
         function execute(accessToken) {
 
-            localStorageService.set("accessToken", accessToken);
-
+            return $http(
+                {
+                    method: "post",
+                    url: config.baseUrl + "/oauth2/token",
+                    data: {
+                        grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+                        assertion: accessToken
+                    }
+                }
+            );
         }
     }
+
 })();
