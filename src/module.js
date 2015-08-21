f3fbe2b73a4c09badcf30989494f898ceb976801
Chris Dostert <chris.dostert@live.com>
2015-08-21 00:45:31 -0700

    added default export

--- src/module.js
+++ src/module.js
@@ -4,11 +4,14 @@ import service from './service';
 import getUserInfoUseCaseFactory from './getUserInfoUseCase.service.js';
 import refreshAccessTokenUseCaseFactory from './refreshAccessTokenUseCase.service.js';
 
-angular
-    .module(
-    'identityServiceSdk.module',
-    [])
-    .provider('identityServiceSdk.config', configProvider)
-    .service('identityServiceSdk', service)
-    .service('identityServiceSdk.getUserInfoUseCase', getUserInfoUseCaseFactory)
-    .service('identityServiceSdk.refreshAccessTokenUseCase', refreshAccessTokenUseCaseFactory);
+const identityServiceSdk =
+    angular
+        .module(
+        'identityServiceSdk.module',
+        [])
+        .provider('identityServiceSdk.config', configProvider)
+        .service('identityServiceSdk', service)
+        .service('identityServiceSdk.getUserInfoUseCase', getUserInfoUseCaseFactory)
+        .service('identityServiceSdk.refreshAccessTokenUseCase', refreshAccessTokenUseCaseFactory);
+
+export default identityServiceSdk;
