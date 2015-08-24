9fa35dc8091827cd193aed3166d543a547d1533b
Chris Dostert <chris.dostert@live.com>
2015-08-24 00:31:09 -0700

    added karma spec reporter plugin

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,7 @@
 module.exports = function (config) {
     config.set({
             frameworks: ['jspm', 'jasmine'],
+            reporters: ["spec"],
             jspm: {
                 loadFiles: ['test/**/*.js'],
                 serveFiles: ['src/**/*.js']
