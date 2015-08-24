e1a2e66f5af78c0695b281cb0019e3ef914ff442
Chris Dostert <chris.dostert@live.com>
2015-08-24 02:39:04 -0700

    changed to phantomjs as elastic bamboo instances don't have deps for chrome

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,12 @@
 module.exports = function (config) {
     config.set({
-            frameworks: ['jspm', 'jasmine'],
+            frameworks: ['jspm', 'jasmine','phantomjs-shim'],
             reporters: ["spec"],
             jspm: {
                 loadFiles: ['test/**/*.js'],
                 serveFiles: ['src/**/*.js']
             },
-            browsers: ['Chrome'],
+            browsers: ['PhantomJS'],
             singleRun: true
         }
     );
