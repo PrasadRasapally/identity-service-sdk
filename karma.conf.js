f40a25a64d0086ada9d75f0a4aae6aebafbd6330
Chris Dostert <chris.dostert@live.com>
2015-09-12 01:33:38 -0700

    updated tests to use sauce labs launcher

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,34 @@
 module.exports = function (config) {
+
+    if (!process.env.SAUCE_USERNAME) {
+        console.log('SAUCE_USERNAME not set');
+        process.exit(1);
+    } else if (!process.env.SAUCE_ACCESS_KEY) {
+        console.log('SAUCE_ACCESS_KEY not set');
+        process.exit(1);
+    }
+
+    const customLaunchers = {
+        sl_chrome: {
+            base: 'SauceLabs',
+            browserName: 'chrome',
+            platform: 'Windows 7',
+            version: 35
+        }
+    };
+
     config.set({
-            frameworks: ['jspm', 'jasmine', 'phantomjs-shim'],
+            frameworks: ['jspm', 'jasmine'],
             reporters: ['spec', 'junit'],
             junitReporter: {
-                outputDir:'test-reports'
+                outputDir: 'test-reports'
             },
             jspm: {
-                loadFiles: ['src/**/*.js','test/**/*.js']
+                loadFiles: ['src/**/*.js', 'test/**/*.js']
             },
-            browsers: ['PhantomJS'],
+            browserNoActivityTimeout: 60000,
+            customLaunchers: customLaunchers,
+            browsers: Object.keys(customLaunchers),
             singleRun: true
         }
     );
